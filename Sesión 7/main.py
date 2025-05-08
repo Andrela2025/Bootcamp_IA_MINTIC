0fd15bf99b45b7693eab7b80bbad7658c7c34254
Andrela2025 <andreapgiraldoo@gmail.com>
2025-05-07 20:36:33 -0500

    abordamos hasta el slide 143

---
+++ Sesión 7/main.py
@@ -0,0 +1,17 @@
+# Escribe esta linea de codigo
+print("Bienvenidos al entrenamiento con python, vamos a disfrutar al maximo esta sesion")
+
+"""
+    descuento en un a compra: si compras mas de $1000, obtienes un descuento del 20%
+    pide el monto de la compra y muestra el precio final    
+"""
+
+# Pedir datos por teclado al usuario int (entero) float (simbolizar decimales) str (cadenas de caracteres) bool (bolenos unos y cero)
+
+compra = float(input("Por favor digita el valor de tu compra: "))
+
+# Si compras mas de $1000 obtienes un decuento del 20%
+# Siempre mas que la salida tenga mas de un camino de resolucion, debo implementar condicionales
+
+# operadores combinados
+# operadaores de asignación =, operadores 
